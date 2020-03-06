7fc616c80af405615fa46eff0c2afb2cba407d23
barthoekstra <barthoekstra@gmail.com>
2020-03-06 12:09:46 +0000

    Finish bulk preprocessor

--- R/preprocess_radar_data.R
+++ R/preprocess_radar_data.R
@@ -1,12 +1,31 @@
+library(tools)
+
 source("R/remove_rays.R")
 source("R/calculate_dpr.R")
 source("R/remove_precipitation.R")
 source("R/remove_groundclutter.R")
 
-preprocess_radar_data <- function(pvol_path, ei_rays, pvol_dynamic_groundclutter, pvol_static_groundclutter) {
+preprocess_radar_data <- function(pvol_path, ei_rays, pvol_dynamic_groundclutter, pvol_static_groundclutter, azim_limits = NULL) {
   pvol <- read_pvolfile(pvol_path, param = "all")
+  
+  # Filter clutter
   pvol <- remove_rays(pvol, rays = ei_rays)
   pvol <- suppressWarnings(calculate_dpr(pvol))
   pvol <- remove_precipitation(pvol)
-  pvol <- remove_groundclutter(remove_groundclutter(pvol, pvol_dynamic_groundclutter), pvol_static_groundclutter)
+  pvol <- remove_groundclutter(pvol, pvol_dynamic_groundclutter)
+  pvol <- remove_groundclutter(pvol, pvol_static_groundclutter)
+  
+  # Calculate VPs
+  if (is.null(azim_limits)) {
+    vp <- calculate_vp(file = pvol_path, vpfile = paste("data/processed/vp/", basename(pvol_path), sep = ""), verbose = FALSE)
+  } else {
+    vp <- calculate_vp(file = pvol_path, vpfile = paste("data/processed/vp/", basename(pvol_path), sep = ""), verbose = FALSE,
+                       azim_min = azim_limits[1], azim_max = azim_limits[2])
+  }
+  
+  # Apply range-bias correction
+  corrected_ppi <- integrate_to_ppi(pvol, vp, res = 500, xlim = c(-150000, 150000), ylim = c(-150000, 150000))
+  
+  # Save resultant PPI
+  saveRDS(corrected_ppi, file = paste("data/processed/corrected-ppis/", basename(file_path_sans_ext(pvol_path)), ".RDS", sep = ""))
 }
