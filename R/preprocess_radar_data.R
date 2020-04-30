3727682255167e711fb0a973c78324898d3fc19a
barthoekstra <barthoekstra@gmail.com>
2020-04-30 06:58:10 +0000

    Make recalculation of vp dependent on overwrite value

--- R/preprocess_radar_data.R
+++ R/preprocess_radar_data.R
@@ -6,7 +6,7 @@ source("R/remove_precipitation.R")
 source("R/remove_groundclutter.R")
 source("R/calculate_distance_to_radar.R")
 
-preprocess_radar_data <- function(pvol_path, ei_rays, pvol_dynamic_groundclutter, pvol_static_groundclutter, azim_limits = NULL) {
+preprocess_radar_data <- function(pvol_path, ei_rays, pvol_dynamic_groundclutter, pvol_static_groundclutter, azim_limits = NULL, overwrite = FALSE) {
   pvol <- read_pvolfile(pvol_path, param = "all")
   
   # Filter clutter
@@ -17,11 +17,16 @@ preprocess_radar_data <- function(pvol_path, ei_rays, pvol_dynamic_groundclutter
   pvol <- remove_groundclutter(pvol, pvol_static_groundclutter)
   
   # Calculate VPs
-  if (is.null(azim_limits)) {
-    vp <- calculate_vp(file = pvol_path, vpfile = paste("data/processed/vp/", basename(pvol_path), sep = ""), verbose = FALSE)
+  vp_out <- paste("data/processed/vp/", basename(pvol_path), sep = "")
+  if (!file.exists(vp_out) || overwrite) {
+    if (is.null(azim_limits)) {
+      vp <- calculate_vp(file = pvol_path, vpfile = vp_out, verbose = FALSE)
+    } else {
+      vp <- calculate_vp(file = pvol_path, vpfile = vp_out, verbose = FALSE,
+                         azim_min = azim_limits[1], azim_max = azim_limits[2])
+    }
   } else {
-    vp <- calculate_vp(file = pvol_path, vpfile = paste("data/processed/vp/", basename(pvol_path), sep = ""), verbose = FALSE,
-                       azim_min = azim_limits[1], azim_max = azim_limits[2])
+    vp <- read_vpfiles(vp_out)
   }
   
   # Apply range-bias correction
@@ -36,5 +41,8 @@ preprocess_radar_data <- function(pvol_path, ei_rays, pvol_dynamic_groundclutter
   corrected_ppi$data$y <- coords[, 2]
   
   # Save resultant PPI
-  saveRDS(corrected_ppi, file = paste("data/processed/corrected-ppis/", basename(file_path_sans_ext(pvol_path)), ".RDS", sep = ""))
+  ppi_out <- paste("data/processed/corrected-ppis/", basename(file_path_sans_ext(pvol_path)), ".RDS", sep = "")
+  saveRDS(corrected_ppi, file = ppi_out)
+  
+  print(ppi_out)
 }
